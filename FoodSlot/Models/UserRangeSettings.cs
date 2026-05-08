fae8af98cd2c7ba0c8b718219da969e379160295
uc <aa89631139@gmail.com>
2026-05-08 15:16:22 +0800

    model3

---
+++ FoodSlot/Models/UserRangeSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodSlot.Models
+{
+    public class UserRangeSettings
+    {
+        [Key]
+        public int RangeSettingsID { get; set; }
+        public int userID { get; set; }
+        public virtual User Users { get; set; }
+        public int rangeID { get; set; }
+        public virtual Range Ranges { get; set; }
+    }
+}
