08d3ab3df01bf48abc530bad133c1a6a3f7df7a7
bread1206-code <bread1206@gmail.com>
2026-05-10 21:34:50 +0800

    建立上傳圖片服務，ImageUploadService

---
+++ FoodSlot/Services/ImageSizeOption.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodSlot.Services
+{
+    //設定模型（Configuration Model）
+    public class ImageSizeOption
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+
+}
