871e63fd0230979023bd701d62616bed18ed3e2b
Liou <a47453386@gmail.com>
2026-05-11 10:15:44 +0800

    SeedData

---
+++ FoodSlot/Seed/Data/SeedUserFoodSettings.cs
@@ -0,0 +1,70 @@
+﻿    using FoodSlot.Models;
+    using Microsoft.EntityFrameworkCore;
+
+namespace FoodSlot.Seed.Data
+{
+    public class SeedUserFoodSettings
+    {
+        private readonly FoodSlotContext _context;
+
+        public SeedUserFoodSettings(FoodSlotContext context)
+        {
+            _context = context;
+        }
+        public void Run()
+        {
+            if (_context.UserFoodSettings.Any()) return;
+
+            _context.UserFoodSettings.AddRange(
+               new UserFoodSettings
+               {
+                   userID = 3,
+                   foodID = 1
+               },
+                 new UserFoodSettings
+                 {
+                     userID = 3,
+                     foodID = 2
+                 },
+                 new UserFoodSettings
+                 {
+                     userID = 3,
+                     foodID = 3
+                 },
+                 new UserFoodSettings
+                 {
+                     userID = 3,
+                     foodID = 4
+                 },
+                 new UserFoodSettings
+                 {
+                     userID = 3,
+                     foodID = 5
+                 }, new UserFoodSettings
+                 {
+                     userID = 3,
+                     foodID = 6
+                 }, new UserFoodSettings
+                 {
+                     userID = 3,
+                     foodID = 7
+                 }, new UserFoodSettings
+                 {
+                     userID = 3,
+                     foodID = 8
+                 }, new UserFoodSettings
+                 {
+                     userID = 3,
+                     foodID = 9
+                 }, new UserFoodSettings
+                 {
+                     userID = 3,
+                     foodID = 10
+                 }
+            );
+            _context.SaveChanges();
+        }
+    }
+}
+        
+ 
---
+++ FoodSlot/Seed/Data/SeedUserRange.cs
@@ -0,0 +1,45 @@
+﻿using FoodSlot.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodSlot.Seed.Data
+{
+    public class SeedUserRange
+    {
+        private readonly FoodSlotContext _context;
+
+        public SeedUserRange(FoodSlotContext context)
+        {
+            _context = context;
+        }
+        public void Run()
+        {
+            if (_context.UserRanges.Any()) return; 
+
+            _context.UserRanges.AddRange(
+               new UserRange
+               {
+                   rangeID = 1,
+                   radius = 500,
+                   createTime = DateTime.Now,
+                   updateTime = null
+               },
+                    new UserRange
+                    {
+                        rangeID = 2,
+                        radius = 1000,
+                        createTime = DateTime.Now,
+                        updateTime = null
+                    },
+                    new UserRange
+                    {
+                        rangeID = 3,
+                        radius = 3000,
+                        createTime = DateTime.Now,
+                        updateTime = null
+                    }
+            );
+            _context.SaveChanges();
+        }
+
+    }
+}
