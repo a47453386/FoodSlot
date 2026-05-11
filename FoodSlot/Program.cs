175d1b2c40688aeee79335952ba3ea177733e24f
Liou <a47453386@gmail.com>
2026-05-11 14:00:23 +0800

    修正

--- FoodSlot/Program.cs
+++ FoodSlot/Program.cs
@@ -5,71 +5,33 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<FoodSlotContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("FoodSlotConnection")));
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-
+//註冊連線字串
+builder.Services.AddDbContext<FoodSlotContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FoodSlotConnection")));
+//註冊服務
+builder.Services.AddScoped<IImageUploadService, ImageUploadService>();
+//註冊種子資料
 builder.Services.AddScoped<SeedRunner>();
+builder.Services.AddScoped<SeedFood>();
 builder.Services.AddScoped<SeedSystemSetting>();
 builder.Services.AddScoped<SeedSystemSettingCategory>();
-builder.Services.AddScoped<SeedFood>();
-builder.Services.AddScoped<SeedUser>();
+builder.Services.AddScoped<SeedUserFoodSettings>();
 builder.Services.AddScoped<SeedUserRange>();
 builder.Services.AddScoped<SeedUserRangeSettings>();
-builder.Services.AddScoped<SeedUserFoodSettings>();
+
 
 
 
 var app = builder.Build();
 
-
-// CLI Seeder 控制（取代原本自動 Seed）
-if (args.Contains("Seed"))
+using (var scope = app.Services.CreateScope())
 {
-    using var scope = app.Services.CreateScope();
-    var services = scope.ServiceProvider;
-
-    var db = services.GetRequiredService<FoodSlotContext>();
-    var baseSeeder = services.GetRequiredService<SeedRunner>();
-    
-
-    bool force = args.Contains("--force");
-
-    bool runBase = args.Contains("base") || (!args.Contains("base") && !args.Contains("demo"));
-    bool runDemo = args.Contains("demo") || (!args.Contains("base") && !args.Contains("demo"));
-
-    Console.WriteLine("=== Seeder Start ===");
-
-    // Base Seed（平台基礎資料）
-    if (runBase)
-    {
-        Console.WriteLine("Running Base Seed...");
-
-        if (!force && db.Users.Any()) // ⚠️ 用你「一定存在的表」
-        {
-            Console.WriteLine("Base Seed 已存在，略過");
-        }
-        else
-        {
-            baseSeeder.Run();
-            Console.WriteLine("Base Seed 完成");
-        }
-    }
-
-    
-
-    Console.WriteLine("=== Seeder End ===");
-
-    return; // 跑完 Seeder 就結束，不啟動網站
+    var seeder = scope.ServiceProvider.GetRequiredService<SeedRunner>();
+    seeder.Run();
 }
-
-
-
-
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
