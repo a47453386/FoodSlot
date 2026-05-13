ab0abf3a4d649d56fd1f292b9c3cea0afabb628b
bread1206-code <bread1206@gmail.com>
2026-05-13 09:45:47 +0800

    建立JavaScript

---
+++ FoodSlot/wwwroot/js/NearBySearch.js
@@ -0,0 +1,20 @@
+﻿const response = await fetch(
+    '/StoreSearch/Nearby',
+    {
+        method: 'POST',
+
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({
+            keyword: '拉麵',
+            latitude: 25.033,
+            longitude: 121.5654,
+            radius: 1500
+        })
+    });
+
+const stores = await response.json();
+
+console.log(stores);
