02893c9e98424436123582520b2c67c1fe761abb
bread1206-code <bread1206@gmail.com>
2026-05-13 13:59:36 +0800

    建立Controller

---
+++ FoodSlot/Controllers/StoreSearchController.cs
@@ -0,0 +1,31 @@
+﻿using FoodSlot.Services.Interfaces;
+using FoodSlot.DTOs;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodSlot.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StoreSearchController : ControllerBase
+    {
+        private readonly IAPIResultService _apiResultService;
+
+        public StoreSearchController(
+            IAPIResultService apiResultService)
+        {
+            _apiResultService = apiResultService;
+        }
+
+        [HttpPost("Nearby")]
+        public async Task<IActionResult> Nearby(
+            [FromBody] NearbySearchRequestDTO request)
+        {
+            var result =
+                await _apiResultService
+                    .NearbySearchAsync(request);
+
+            return Ok(result);
+        }
+    }
+}
