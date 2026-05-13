9ae8a5988cd8eb2994533fe26465bc3f712111d2
bread1206-code <bread1206@gmail.com>
2026-05-13 09:44:59 +0800

    建立Interface

---
+++ FoodSlot/Services/Interface/IAPIResultService.cs
@@ -0,0 +1,10 @@
+﻿using FoodSlot.DTOs;
+
+namespace FoodSlot.Services.Interfaces
+{
+    public interface IAPIResultService
+    {
+        Task<List<StoreDTO>> NearbySearchAsync(
+            NearbySearchRequestDTO request);
+    }
+}
