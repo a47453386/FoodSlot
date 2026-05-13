53eef187c7b5a5a9f57a82b8fa42f983d0025437
bread1206-code <bread1206@gmail.com>
2026-05-13 09:44:41 +0800

    建立DTO

---
+++ FoodSlot/DTOs/StoreDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace FoodSlot.DTOs
+{
+    public class StoreDTO
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public double Rating { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+}
