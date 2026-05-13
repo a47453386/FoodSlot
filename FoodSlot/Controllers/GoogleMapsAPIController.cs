5972a6648c98d7985f382e2d03880282e0d284a8
bread1206-code <bread1206@gmail.com>
2026-05-13 14:00:16 +0800

    建立Swagger測試環境

---
+++ FoodSlot/Controllers/GoogleMapsAPIController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FoodSlot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoogleMapsAPIController : ControllerBase
+    {
+        // GET: api/<GoogleMapsAPIController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<GoogleMapsAPIController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<GoogleMapsAPIController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<GoogleMapsAPIController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<GoogleMapsAPIController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
