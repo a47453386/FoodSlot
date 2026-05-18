a4828be6921ecb8c62621aa0656da2d9f5be8351
bread1206-code <bread1206@gmail.com>
2026-05-18 19:25:34 +0800

    更新NearBySearch.js，修改忽略列表

--- FoodSlot/wwwroot/js/NearBySearch.js
+++ FoodSlot/wwwroot/js/NearBySearch.js
@@ -1,20 +1,44 @@
-﻿const response = await fetch(
-    '/StoreSearch/Nearby',
-    {
-        method: 'POST',
+﻿async function testApi(keyword, latitude, longitude, radius) {
 
-        headers: {
-            'Content-Type': 'application/json'
-        },
+    const response =
+        await fetch('/StoreSearch/Nearby', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
 
-        body: JSON.stringify({
-            keyword: '拉麵',
-            latitude: 25.033,
-            longitude: 121.5654,
-            radius: 1500
-        })
+            body: JSON.stringify({
+                keyword: keyword,
+                latitude: latitude, 
+                longitude: longitude,
+                radius: radius
+            })
+        });
+
+    const stores =
+        await response.json();
+
+    console.log(stores);
+
+    let html = '';
+
+    stores.forEach(store => {
+
+        html += `
+                <div>
+                    <h3>${store.name}</h3>
+                    <p>${store.address}</p>
+                    <p>${store.rating}</p>
+                    <img src="${store.photoUrl}"/>
+                    <a href="${store.googleMapUrl}" target="_blank" rel="noopener noreferrer">
+                    導航
+                    </a>
+                </div>
+            `;
     });
-
-const stores = await response.json();
-
-console.log(stores);
+    /*target="_blank" 開新分頁 */
+    /*rel="noopener noreferrer" 安全性保護*/
+    document
+        .getElementById('result')
+        .innerHTML = html;
+}
