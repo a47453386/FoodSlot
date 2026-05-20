bbbcc5ac629a1e38188e26415c5e0958ce94c10e
bread1206-code <bread1206@gmail.com>
2026-05-20 09:09:38 +0800

    建立DrawController，API

---
+++ FoodSlot/Areas/API/Controllers/DrawController.cs
@@ -0,0 +1,43 @@
+﻿using FoodSlot.Interfaces;
+using FoodSlot.ViewModels.Slot;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace FoodSlot.Areas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DrawController : ControllerBase
+    {
+        private readonly IDrawService _drawService;
+        public DrawController(IDrawService drawService)
+        {
+            _drawService = drawService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Start()
+        {
+            //預設
+            int? userID = null;
+
+            //判斷是否登入，有登入時，取得登入者的userID字串
+            if (User.Identity?.IsAuthenticated == true)
+            {
+                string? userIDString =
+                    User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                //userIDString不是空值時，將userID字串轉成int
+                if (!string.IsNullOrWhiteSpace(userIDString))
+                {
+                    userID = int.Parse(userIDString);
+                }
+            }
+
+            //將DrawAsync()執行結果存到result
+            VMSlotDrawResult result =
+                await _drawService.DrawAsync(userID);
+
+            return Ok(result);
+        }
+    }
+}
